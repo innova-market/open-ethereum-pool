aa044fc44db5cec8da5dbcf852cc239789b891cf
35pool <cifrano.rf@gmail.com>
2017-08-12 21:01:31 +0300

    Update server.go

--- api/server.go
+++ api/server.go
@@ -12,8 +12,8 @@ import (
 
 	"github.com/gorilla/mux"
 
-	"github.com/sammy007/open-ethereum-pool/storage"
-	"github.com/sammy007/open-ethereum-pool/util"
+	"github.com/innova-market/open-ethereum-pool/storage"
+	"github.com/innova-market/open-ethereum-pool/util"
 )
 
 type ApiConfig struct {
