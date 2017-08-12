b5a033eea9380cc1fb8184a6834bb6f92aa52a21
35pool <cifrano.rf@gmail.com>
2017-08-12 21:25:51 +0300

    Update unlocker_test.go

--- payouts/unlocker_test.go
+++ payouts/unlocker_test.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/sammy007/open-ethereum-pool/rpc"
-	"github.com/sammy007/open-ethereum-pool/storage"
+	"github.com/innova-market/open-ethereum-pool/rpc"
+	"github.com/innova-market/open-ethereum-pool/storage"
 )
 
 func TestMain(m *testing.M) {
